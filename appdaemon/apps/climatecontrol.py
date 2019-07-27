a997b5bed6c4beb0d83279f67cd295c11471f2c6
Willem van Asperen <willem@van.asperen.org>
2019-07-27 08:05:51 +0200

    storing target boiler state in binary sensor state

--- appdaemon/apps/climatecontrol.py
+++ appdaemon/apps/climatecontrol.py
@@ -11,6 +11,7 @@ class SmartCV(hass.Hass):
 		self.set_point_pattern = re.compile('[^\\d]*([\\d]+|[\\d]+\\.[\\d]*)')
 		self.calendar = self.args['calendar']
 		self.boiler = self.args['boiler']
+		self.boiler_target = 'binary_sensor.boiler_target'
 		self.zones = self.args['zones']
 
 		self._initialize_state()
@@ -74,20 +75,27 @@ class SmartCV(hass.Hass):
 			self.log("Switching off {}".format(controls), level="INFO")
 			self.call_service('climate/turn_off', entity_id=controls)
 
-	def _get_required_boiler_state(self):
+	def update_boiler_target_cb(self, entity, attribute, old, new, kwargs):
+		self.log("Got a state change from {}, updating target boiler state now".format(entity), level="INFO")
+		reasons = []
 		for zone in self.zones:
 			for control in zone['controls']:
 				if self.get_state(control) != 'off':
 					current_temperature = self.get_state(control, attribute="current_temperature")
-					target_temperature = self.get_state(control, attribute="temperature")
-					if current_temperature is not None and target_temperature is not None:
-						if current_temperature < target_temperature:
-							return 'on'
-		return 'off'
-
-	def update_boiler_target_cb(self, entity, attribute, old, new, kwargs):
-		self.log("Got a state change from {}, updating target  boiler state now".format(entity), level="INFO")
-		self.boiler_target_state = self._get_required_boiler_state()
+					if current_temperature is not None:
+						target_temperature = self.get_state(control, attribute="temperature")
+						if target_temperature is not None:
+							if current_temperature < target_temperature:
+								reasons.append(dict(
+									zone=zone["name"],
+									control=control,
+									current_temperature=current_temperature,
+									target_temperature=target_temperature
+								))
+		if len(reasons) > 0:
+			self.set_state(self.boiler_target, state='on', attributes=dict(trigger=entity, reasons=reasons))
+		else:
+			self.set_state(self.boiler_target, state='off', attributes=dict(trigger=entity))
 
 	def update_boiler_cb(self, kwargs):
 		current_state = self.get_state(self.boiler)
@@ -95,8 +103,13 @@ class SmartCV(hass.Hass):
 			self.log("Could not determine current state of boiler with entity id {}".format(self.boiler), level="WARNING")
 			return
 
-		if current_state != self.boiler_target_state:
-			self.log("Switching boiler from {} to {}".format(current_state, self.boiler_target_state))
+		target_state = self.get_state(self.boiler_target)
+		if target_state is None:
+			self.log("Could not determine target state of boiler with entity id {}".format(self.boiler_target), level="WARNING")
+			return
+
+		if current_state != target_state:
+			self.log("Switching boiler from {} to {}".format(current_state, target_state))
 			self.toggle(self.boiler)
 		else:
 			self.log("No need to change the boiler state", level="DEBUG")
