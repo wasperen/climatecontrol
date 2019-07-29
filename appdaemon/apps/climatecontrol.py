4cf8d755e0839827ca5beb63969220868187e6f9
Willem van Asperen <willem@van.asperen.org>
2019-07-29 09:22:09 +0200

    now scheduling the off when state=on comes in. plus clean-up

--- appdaemon/apps/climatecontrol.py
+++ appdaemon/apps/climatecontrol.py
@@ -1,6 +1,9 @@
 import re
 import datetime
+from iso8601 import parse_date, ParseError
 import appdaemon.plugins.hass.hassapi as hass
+
+DEFAULT_BOILER_TARGET_ENTITY = 'binary_sensor.boiler_target'
 
 
 class SmartCV(hass.Hass):
@@ -11,24 +14,22 @@ class SmartCV(hass.Hass):
 		self.set_point_pattern = re.compile('[^\\d]*([\\d]+|[\\d]+\\.[\\d]*)')
 		self.calendar = self.args['calendar']
 		self.boiler = self.args['boiler']
-		self.boiler_target = 'binary_sensor.boiler_target'
+		self.boiler_target = self.args.get('boiler_target', DEFAULT_BOILER_TARGET_ENTITY)
 		self.zones = self.args['zones']
 
 		self._initialize_state()
 
-		self.listen_state(self.calendar_cb, self.calendar, attribute="all")
+		self.listen_state(self.calendar_cb, self.calendar, attribute="all", new='on')
 		self.run_minutely(self.update_boiler_cb, datetime.time(0, 0, 0))
 
 	def _initialize_state(self):
-		# TODO we should obtain the current state from the calendar using the hass api
-		# for now we just put everything off
+		# TODO we should obtain the current state from the calendar using the hass api; for now switch everything off
 		self.turn_off(self.boiler)
-		self.boiler_target_state = 'off'
+		self.set_state(self.boiler_target, state="off", attributes=dict(trigger="initialize"))
 
-		for zone in self.zones:
-			for control in zone['controls']:
-				self.turn_off(control)
-				self.listen_state(self.update_boiler_target_cb, control)
+		for control in set([c for z in self.zones for c in z['controls']]):
+			self.turn_off(control)
+			self.listen_state(self.update_boiler_target_cb, control)
 
 	def _get_controls(self, calendar_message):
 		for zone in self.zones:
@@ -47,11 +48,21 @@ class SmartCV(hass.Hass):
 		return controls, set_point
 
 	def calendar_cb(self, entity, attribute, old_state, new_state, kwargs):
-		if "attributes" not in new_state or "message" not in new_state["attributes"]:
+		if new_state["state"] != 'on':
+			self.log("Received calendar event with new state {}".format(new_state["state"]), level="DEBUG")
+			return
+
+		if "attributes" not in new_state:
+			self.log("Got calendar state change with no attributes: {}".format(new_state), level="WARNING")
+			return
+
+		attributes = new_state["attributes"]
+
+		if "message" not in attributes:
 			self.log("Got a calendar state change with no message attribute: {}".format(new_state), level="WARNING")
 			return
 
-		message = new_state["attributes"]["message"]
+		message = attributes["message"]
 		if message is None or message == "":
 			self.log("Got a calendar state change with an empty message", level="WARNING")
 			return
@@ -67,40 +78,48 @@ class SmartCV(hass.Hass):
 			self.log("Unrealistic set point at {}".format(set_point), level="WARNING")
 			return
 
-		if new_state["state"] == 'on':
-			self.log("Setting {} to {}".format(controls, set_point), level="INFO")
-			self.call_service('climate/set_temperature', entity_id=controls, temperature=set_point)
-			self.call_service('climate/turn_on', entity_id=controls)
-		else:
-			self.log("Switching off {}".format(controls), level="INFO")
-			self.call_service('climate/turn_off', entity_id=controls)
+		if "end_time" not in attributes:
+			self.log("Calendar event attributes do not contain end_time: {}".format(new_state), level="WARNING")
+			return
+
+		try:
+			switch_off_time = parse_date(attributes["end_time"]) - datetime.timedelta(minutes=-1)
+		except ParseError as error:
+			self.log("Failure parsing datetime from calendar event end_time: {}".format(attributes["end_time"]), level="WARNING")
+			self.log("Parse Error: {}".format(error), level="WARNING")
+			return
+
+		self.log("Setting {} to {} and plan to switch off at {}".format(controls, set_point, switch_off_time), level="INFO")
+		self.call_service('climate/set_temperature', entity_id=controls, temperature=set_point)
+		self.call_service('climate/turn_on', entity_id=controls)
+
+		self.run_at(self.timed_off_cb(), switch_off_time)
+
+	def timed_off_cb(self, controls):
+		self.log("Switching off {}".format(controls), level="INFO")
+		self.call_service('climate/turn_off', entity_id=controls)
 
 	def update_boiler_target_cb(self, entity, attribute, old, new, kwargs):
 		self.log("Got a state change from {}, updating target boiler state now".format(entity), level="INFO")
 		reasons = {}
-		for zone in self.zones:
-			for control in zone['controls']:
-				if self.get_state(control) == 'off':
-					continue
+		for (zone_name, control) in [(z["name"], c) for z in self.zones for c in z['controls']]:
+			if self.get_state(control) == 'off':
+				continue
 
-				current_temperature = self.get_state(control, attribute="current_temperature")
-				if current_temperature is None:
-					self.log("Could not determine current temperature from {}".format(control), level="WARNING")
-					continue
+			current_temperature = self.get_state(control, attribute="current_temperature")
+			if current_temperature is None:
+				self.log("Could not determine current temperature from {}".format(control), level="WARNING")
+				continue
 
-				target_temperature = self.get_state(control, attribute="temperature")
-				if target_temperature is None:
-					self.log("Could not determine target temperature for {}".format(control), level="WARNING")
-					continue
+			target_temperature = self.get_state(control, attribute="temperature")
+			if target_temperature is None:
+				self.log("Could not determine target temperature for {}".format(control), level="WARNING")
+				continue
 
-				if current_temperature < target_temperature:
-					zone_name = zone["name"]
-					if zone_name not in reasons:
-						reasons[zone_name] = []
-					reasons[zone_name].append(dict(
-						control=control,
-						reason="{}<{}".format(current_temperature, target_temperature)
-					))
+			if current_temperature < target_temperature:
+				reasons[zone_name] = reasons.get(zone_name, default=[]).append(
+					{control: "{}<{}".format(current_temperature, target_temperature)}
+				)
 
 		if len(reasons) > 0:
 			self.log("Setting target boiler state to on, reasons: {}".format(reasons), level="INFO")
